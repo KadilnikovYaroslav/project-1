a748f5ce7d8b0f1c56361d5aa6138b101ab8ca18
Yaroslav K <expected90@mail.ru>
2015-12-10 19:54:14 +0200

    work on the bugs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-git clone https://github.com/KadilnikovYaroslav/project-1.git project-1
-cd project-1
-bower i
-npm i
-gulp
+1. git clone https://github.com/KadilnikovYaroslav/project-1.git
+2. cd project-1
+3. bower i
+4. npm i
+5. gulp
--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,57 @@
- $ (function (){
- 	if(Modernizr.input.placeholder){
- 	$('input, textarea').placeholder
+ $(document).ready(function() {
+
+ 	if(!Modernizr.input.placeholder){
+ 		$('input, textarea').placeholder();
  	}
- });
+
+});
 
 
+$(document).ready(function() {
+    $(".overlay").css({opacity: 0.5});
+    positionCenter($(".popup"));
+    
+//    $('.site-add').on('click'.function(e){
+//        e.preventDefault();         
+//    });
+    
+    $(".add-project").click(function() {
+        $(".popup, .overlay").fadeIn(500);
+    });
+    
+    $(".close-popup").click(function() {
+       $(".popup, .overlay").fadeOut(500); 
+    });
+    
+    function positionCenter(elem) {
+        elem.css({
+            marginTop: '-' + elem.height() / 2 + 'px',
+            marginLeft: '-' + elem.width() / 2 + 'px'
+        });
+    }
+});
+
+
+//var validation = (function () {
+//    
+//    var    
+//        _init = function () {
+//            _setUpListeners();  
+//        },
+//    
+//        _setUpListeners = function () {
+//            
+//        },
+//    
+//    return {
+//        init: _init
+//    }
+//    
+//    
+//}());
+//
+//     validation.init();
+//
+//        $
+//});
 
